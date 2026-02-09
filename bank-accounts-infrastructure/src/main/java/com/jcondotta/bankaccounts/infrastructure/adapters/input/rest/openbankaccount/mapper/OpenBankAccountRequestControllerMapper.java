33ee1289a60ebcc296394fc3eb475ff46c77cfac
Jefferson Condotta <jefferson-william.feitosa-condotta@capgemini.com>
2026-02-09 12:18:11 +0100

    creating tests for open bank account controller

--- bank-accounts-infrastructure/src/main/java/com/jcondotta/bankaccounts/infrastructure/adapters/input/rest/openbankaccount/mapper/OpenBankAccountRequestControllerMapper.java
+++ bank-accounts-infrastructure/src/main/java/com/jcondotta/bankaccounts/infrastructure/adapters/input/rest/openbankaccount/mapper/OpenBankAccountRequestControllerMapper.java
@@ -13,25 +13,10 @@ import org.mapstruct.Named;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OpenBankAccountRequestControllerMapper {
 
-    @Mapping(target = "accountHolderName", source = "accountHolder.accountHolderName", qualifiedByName = "toAccountHolderName")
-    @Mapping(target = "passportNumber", source = "accountHolder.passportNumber", qualifiedByName = "toPassportNumber")
-    @Mapping(target = "dateOfBirth", source = "accountHolder.dateOfBirth", qualifiedByName = "toDateOfBirth")
+    @Mapping(target = "accountHolderName", source = "accountHolder.accountHolderName")
+    @Mapping(target = "passportNumber", source = "accountHolder.passportNumber")
+    @Mapping(target = "dateOfBirth", source = "accountHolder.dateOfBirth")
     @Mapping(target = "accountType", source = "accountType")
     @Mapping(target = "currency", source = "currency")
     OpenBankAccountCommand toCommand(OpenBankAccountRequest request);
-
-    @Named("toAccountHolderName")
-    default AccountHolderName toAccountHolderName(String value) {
-        return AccountHolderName.of(value);
-    }
-
-    @Named("toPassportNumber")
-    default PassportNumber toPassportNumber(String value) {
-        return PassportNumber.of(value);
-    }
-
-    @Named("toDateOfBirth")
-    default DateOfBirth toDateOfBirth(java.time.LocalDate value) {
-        return DateOfBirth.of(value);
-    }
 }
