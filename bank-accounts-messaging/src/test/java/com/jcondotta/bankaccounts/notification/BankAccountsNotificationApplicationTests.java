0873d7d24cdeaa26f8521e6bd3105efafe027479
jcondotta <jefferson.condotta@gmail.com>
2026-03-09 11:47:59 +0100

    Add initial POM files and application properties for core and infrastructure modules

---
+++ bank-accounts-messaging/src/test/java/com/jcondotta/bankaccounts/notification/BankAccountsNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcondotta.bankaccounts.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountsNotificationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
